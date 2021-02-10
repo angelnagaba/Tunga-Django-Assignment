c1b1b60e97dae3ebfa32633188d4b9c9e875abd6
angelnagaba <angelnagaba99@gmail.com>
2021-02-10 14:26:48 -0500

    Made a few more changes

--- profiles/views.py
+++ profiles/views.py
@@ -6,10 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 from django.urls import reverse_lazy
 from .forms import ProfileForm,TemperatureForm
 from .models import profiles, Temperature
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework import filters
+
 
 
 
